ca0b33c6fbadd100c45818b0f88e8c4c75f55cca
olinex <joshua.olin@163.com>
2017-11-03 16:45:12 +0800

    unable the messages app

--- django_erp/settings.py
+++ django_erp/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
+    # 'django.contrib.messages',
     'django.contrib.staticfiles',
 
     'channels',
